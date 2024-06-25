d866e7c4eb104a1e50602e9cebcb1be964912e9b
Beyza Doğan <beyzaibsal@gmail.com>
2024-06-25 15:23:12 +0300

    Last2 Update fot Netlify

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -6,8 +5,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['react-bootstrap'],
-      external: ['@fortawesome/react-fontawesome']
+      external: ['react-bootstrap', '@fortawesome/react-fontawesome']
     }
   }
 });
